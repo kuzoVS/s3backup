5051503d7e489d899c4b31c07d312e4ce9d1ed35
Saimon <56156971+kuzoVS@users.noreply.github.com>
2026-03-27 06:37:39 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials <<EOF
+[default]
+aws_access_key_id=${S3_ACCESS_KEY_ID}
+aws_secret_access_key=${S3_SECRET_ACCESS_KEY}
+EOF
+
+cat > ~/.aws/config <<EOF
+[default]
+region=${S3_REGION}
+EOF
+
+if [ -n "${SCHEDULE}" ]; then
+    echo "Setting up cron: ${SCHEDULE}"
+    env >> /etc/environment
+    echo "${SCHEDULE} . /etc/environment; /backup.sh >> /var/log/backup.log 2>&1" | crontab -
+    crond -f -l 2
+else
+    echo "No SCHEDULE set, running once..."
+    /backup.sh
+fi
